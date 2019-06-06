79685a1ee697d62e98c4a1f0bba7e0fc1508e85c
James Madden <jamesbmadden@gmail.com>
2019-06-06 11:27:44 -0700

    ww-timeline component

---
+++ src/timeline.js
@@ -0,0 +1,14 @@
+import { LitElement, html, customElement, property, css } from 'https://unpkg.com/lit-element@2.1.0/lit-element.js?module';
+
+@customElement('ww-timeline')
+export default class Timeline extends LitElement {
+
+  render () {
+    return html`
+      <div class="timeline-container">
+        <p>Coming Soon</p>
+      </div>
+    `;
+  }
+
+}
