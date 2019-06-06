2457eca76993a13323180ff83cec3159d8361d97
James Madden <jamesbmadden@gmail.com>
2019-06-06 11:36:51 -0700

    Add colour themes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './timeline.js';
+
+window.colours = [
+  {
+    "background": "#222",
+    "text": "#fff",
+    "primary": "#fff"
+  },
+  {
+    "background": "#fff",
+    "text": "#222",
+    "primary": "#222"
+  },
+  {
+    "background": "#efda9e",
+    "text": "#444",
+    "primary": "#b71c1c"
+  }
+];
+
+document.body.style.background = colours[2].background;
+document.body.style.color = colours[2].text;
