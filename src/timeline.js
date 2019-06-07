eb963ed6fc87d1f0a3314b6d7495f747217d3605
James Madden <jamesbmadden@gmail.com>
2019-06-06 17:30:28 -0700

    Add basic styles

--- src/timeline.js
+++ src/timeline.js
@@ -7,10 +7,30 @@ export default class Timeline extends LitElement {
   @property({type: Date}) end;
   @property({type: Array}) items;
 
+  static get styles () {
+    return css`
+      .timeline-container {
+        position: relative;
+        height: 100%;
+        width: 100%;
+        overflow-x: auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .timeline-inner {
+        max-width: 720px;
+        width: 100%;
+      }
+    `;
+  }
+
   render () {
     return html`
       <div class="timeline-container">
-        <p>Coming Soon</p>
+        <div class="timeline-inner">
+          <p>Coming Soon</p>
+        </div>
       </div>
     `;
   }
