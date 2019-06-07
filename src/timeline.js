19274acc9614881c6852e497e69a7efb213f27a0
James Madden <jamesbmadden@gmail.com>
2019-06-06 17:02:28 -0700

    Add properties to timeline

--- src/timeline.js
+++ src/timeline.js
@@ -3,11 +3,26 @@ import { LitElement, html, customElement, property, css } from 'https://unpkg.co
 @customElement('ww-timeline')
 export default class Timeline extends LitElement {
 
+  @property({type: Date}) start;
+  @property({type: Date}) end;
+  @property({type: Array}) items;
+
   render () {
     return html`
       <div class="timeline-container">
         <p>Coming Soon</p>
       </div>
+    `;
+  }
+
+}
+
+@customElement('ww-timeline-item')
+export class TimelineItem extends LitElement {
+
+  render () {
+    return html`
+    
     `;
   }
 
