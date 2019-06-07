0358f322687f06fab9e08bbd35a7ac670c5f7aa8
James Madden <jamesbmadden@gmail.com>
2019-06-06 17:52:32 -0700

    Add Comments and code to add events to timeline

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './timeline.js';
 
+/*
+  Create colour scheme. window.colours and window.theme can be referenced anywhere for the selected colour pallete. This is for easy
+  editing and comparing.
+*/
 window.theme = 2;
 
 window.colours = [
@@ -22,3 +26,29 @@ window.colours = [
 
 document.body.style.background = colours[theme].background;
 document.body.style.color = colours[theme].text;
+
+/*
+  Provide Objects to the timeline component. This could be done in html, but is much simpler in JavaScript.
+*/
+const timeline = document.querySelector('ww-timeline');
+
+timeline.start = new Date(2004, 0, 19);
+timeline.end = new Date();
+timeline.items = [
+  {
+    date: new Date(2005, 0, 19),
+    title: 'First Birthday'
+  },
+  {
+    date: new Date(2007, 0, 19),
+    title: 'Third Birthday'
+  },
+  {
+    date: new Date(2012, 0, 19),
+    title: 'Eighth Birthday'
+  },
+  {
+    date: new Date(2019, 0, 19),
+    title: 'Fifteenth Birthday'
+  }
+];
