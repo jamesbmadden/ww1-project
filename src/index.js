2cbe1bfbcda9459590e3b89cb06b75a9f85decab
James Madden <jamesbmadden@gmail.com>
2019-06-12 11:23:40 -0700

    Replace Images

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './timeline.js';
+import './dialogue.js';
 
 /*
   Create colour scheme. window.colours and window.theme can be referenced anywhere for the selected colour pallete. This is for easy
@@ -39,58 +40,58 @@ timeline.items = [
   {
     title: 'Canada\'s Entry',
     date: new Date(1914, 6, 28),
-    image: './img/galapagos.jpg'
+    image: './img/war.jpg'
   },
   {
     title: 'The Second Battle of Ypres',
     date: new Date(1915, 3, 22),
-    image: './img/tobermory.jpg'
+    image: './img/ypres.jpg'
   },
   {
     title: 'Battle of Festubert',
     date: new Date(1915, 4, 15),
-    image: './img/Paris.jpg'
+    image: './img/festubert.jpg'
     // 15–25 May 1915
   },
   {
     title: 'Battle St. Eloi Craters',
     date: new Date(1916, 2, 27),
-    image: './img/Duart.jpg'
+    image: './img/eloi.jpg'
   },
   {
     title: 'Battle Mount Sorrel',
     date: new Date(1916, 5, 2),
-    image: './img/berlin.jpg'
+    image: './img/sorrel.jpg'
   },
   {
     title: 'The Battle of the Somme',
     date: new Date(1916, 10, 18),
-    image: './img/Paris.jpg'
+    image: './img/somme.jpg'
   },
   {
     title: 'Vimy Ridge',
     date: new Date(1917, 3, 9),
-    image: './img/tobermory.jpg'
+    image: './img/vimy.jpg'
   },
   {
     title: 'Battle of Hill 70',
     date: new Date(1917, 7, 15),
-    image: './img/berlin.jpg'
+    image: './img/hill70.jpg'
   },
   {
     title: 'Battle of Passchendaele',
     date: new Date(1917, 10, 10),
-    image: './img/galapagos.jpg'
+    image: './img/passchendaele.jpeg'
   },
   {
     title: 'Defense of the German Offensives',
     date: new Date(1918, 2, 21),
-    image: './img/Duart.jpg'
+    image: './img/spring.jpg'
   },
   {
-    title: 'More Independant Canada',
+    title: 'More Independent Canada',
     date: new Date(1918, 10, 11),
-    image: './img/Paris.jpg'
+    image: './img/victory.jpg'
   }
 ];
 /*
