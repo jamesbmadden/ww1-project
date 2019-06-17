b2a08e9bbcec6b0e70e847216d2acfb06e567889
James Madden <jamesbmadden@gmail.com>
2019-06-17 16:04:18 -0700

    Move colour scheme to html and remove colour scheme code

--- src/index.js
+++ src/index.js
@@ -2,33 +2,6 @@ import './timeline.js';
 import './dialogue.js';
 
 /*
-  Create colour scheme. window.colours and window.theme can be referenced anywhere for the selected colour pallete. This is for easy
-  editing and comparing.
-*/
-window.theme = 0;
-
-window.colours = [
-  {
-    "background": "#222",
-    "text": "#fff",
-    "primary": "#fff"
-  },
-  {
-    "background": "#fff",
-    "text": "#222",
-    "primary": "#222"
-  },
-  {
-    "background": "#efda9e",
-    "text": "#444",
-    "primary": "#b71c1c"
-  }
-];
-
-document.body.style.background = colours[theme].background;
-document.body.style.color = colours[theme].text;
-
-/*
   Provide Objects to the timeline component. This could be done in html, but is much simpler in JavaScript.
 */
 const timeline = document.querySelector('ww-timeline');
