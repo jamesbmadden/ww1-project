4ac18413af01ce677d8466eaf0feb5039e83c7af
James Madden <jamesbmadden@gmail.com>
2019-06-19 18:57:36 -0700

    Remove date comments

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import './dialogue.js';
 */
 const timeline = document.querySelector('ww-timeline');
 
-// July 28, 1914 – November 11, 1918
 timeline.start = new Date(1914, 6, 28);
 timeline.end = new Date(1918, 10, 11);
 timeline.items = [
@@ -27,7 +26,6 @@ timeline.items = [
     title: 'Battle of Festubert',
     date: new Date(1915, 4, 15),
     image: './img/festubert.jpg'
-    // 15–25 May 1915
   },
   {
     title: 'Battle St. Eloi Craters',
